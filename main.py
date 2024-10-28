bd3f7f391cc81f4393b64eb7842c4a12f2cf7cf6
Patrick Church <143661018+patrick-church@users.noreply.github.com>
2024-10-28 12:32:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-#from deck_generation import generate_data
-#from simulation import run_simulation
+from deck_generation import generate_data
+from simulation import run_simulation
 
 def main():
     num_iterations = 50000  # Number of decks to generate and rounds to simulate
